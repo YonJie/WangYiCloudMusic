b43dcd03edad4a133a3f4cf9efa04b194876dbdc
@cai_yong_jie <1969764749@qq.com>
2023-08-15 15:25:07 +0800

    路由初始化

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
+import { HashRouter } from 'react-router-dom'
+import '@/assets/css/index.less'
+import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 )
